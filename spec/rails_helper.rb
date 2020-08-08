2d51ee801d3d243782664166389a0d3c66fad418
kunoyasu <kunoyasuyuki@yahoo.co.jp>
2020-08-08 21:46:29 +0900

    結合テストの実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
-
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
@@ -40,10 +40,10 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  config.include SignInSupport
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
-
+  
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
